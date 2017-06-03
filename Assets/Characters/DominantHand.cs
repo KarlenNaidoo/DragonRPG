7d81ffd7c254906f73ac4e90091581b48a00f00c
Unknown <karlen.naidoo@gmail.com>
2017-06-03 16:11:47 +1000

    Lecture 79

---
+++ Assets/Characters/DominantHand.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DominantHand : MonoBehaviour {
+
+    // Add to dominant hand on character prefab
+
+    void Start()
+    {
+        print("Hello!");
+    }
+}
--- Assets/Characters/Player/Player.cs
+++ Assets/Characters/Player/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 [RequireComponent(typeof(CameraRaycaster))]
@@ -14,8 +15,8 @@ public class Player : MonoBehaviour, IDamageable {
     [SerializeField] float minTimeBetweenHits = .5f;
     [SerializeField] float maxAttackRange = 2f;
     [SerializeField] Weapon weaponInUse;
-    [SerializeField] GameObject weaponSocket;
 
+    
     GameObject currentTarget;
     CameraRaycaster cameraRaycaster;
     float lastHitTime = 0f;
@@ -31,9 +32,20 @@ public class Player : MonoBehaviour, IDamageable {
     private void PutWeaponInHand()
     {
         var weaponPrefab = weaponInUse.GetWeaponPrefab();
-        var weapon = Instantiate(weaponPrefab, weaponSocket.transform);
+        GameObject dominantHand = RequestDominantHand();
+        var weapon = Instantiate(weaponPrefab, dominantHand.transform);
         weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
         weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
+    }
+
+    private GameObject RequestDominantHand()
+    {
+        var dominantHands = GetComponentsInChildren<DominantHand>();
+        int numberOfDominantHands = dominantHands.Length;
+        Assert.IsFalse(numberOfDominantHands <= 0, "No DominantHand found on player, please add one");
+        Assert.IsFalse(numberOfDominantHands > 1, "Multiple DominantHand scripts on Player, please remove one");
+        return dominantHands[0].gameObject;
+
     }
 
     private void RegisterForMouseClick()
@@ -42,6 +54,7 @@ public class Player : MonoBehaviour, IDamageable {
         cameraRaycaster.notifyMouseClickObservers += OnMouseClick;
     }
 
+    // Refactor to simplify, reduce number of lines
     void OnMouseClick(RaycastHit raycastHit, int layerHit)
     {
         if (layerHit == enemyLayer)
