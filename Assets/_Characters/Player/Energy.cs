0caca3969501d26f4a68641ed34a7dbdc880c8b1
Unknown <karlen.naidoo@gmail.com>
2017-06-11 13:01:26 +1000

    Making energy bar work at right click

---
+++ Assets/_Characters/Player/Energy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using RPG.CameraUI;
+
+namespace RPG.Characters
+{
+
+    public class Energy : MonoBehaviour
+    {
+        [SerializeField] RawImage energyBar;
+        [SerializeField] float maxEnergyPoints = 100f;
+        [SerializeField] float pointsPerHit = 10f;
+        CameraRaycaster cameraRaycaster;
+
+        float currentEnergyPoints;
+        private float newEnergyPoints;
+
+        // Use this for initialization
+        void Start()
+        {
+            currentEnergyPoints = maxEnergyPoints;
+            cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+            cameraRaycaster.notifyRightClickObservers += ProcessRightClick;
+        }
+
+        void ProcessRightClick(RaycastHit raycastHit, int layerHit)
+        {
+            newEnergyPoints = (currentEnergyPoints - pointsPerHit);
+            currentEnergyPoints = Mathf.Clamp(newEnergyPoints, 0, maxEnergyPoints);
+            UpdateEnergyBar();
+        }
+
+        private void UpdateEnergyBar()
+        {
+            float xValue = -(EnergyAsPercent() / 2f) - 0.5f;
+            energyBar.uvRect = new Rect(xValue, 0f, 0.5f, 1f);
+        }
+
+        float EnergyAsPercent()
+        {
+            return  currentEnergyPoints / maxEnergyPoints; 
+        }
+    }
+
+}
